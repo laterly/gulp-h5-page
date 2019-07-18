dadea4c76ebaae621cc1cc62e4e98d21822ec84b
laterly <1332938125@qq.com>
2019-07-18 16:31:12 +0800

    fix

--- src/front/index/js/index.js
+++ src/front/index/js/index.js
@@ -1,7 +1,8 @@
 console.log('index');
 console.log('版本号');
-// let foo = require('../../utils/foo.js')
-// console.log(foo);  // Object
-// console.log(foo.variable); // 8
-// console.log(foo.sum(1)); // 7
-// let foo =require('../../utils/foo.js');
+// let bar=require('../../utils/foo')
+// console.log(bar.variable);
+// import FlashMessage from "../../utils/play.js";
+
+// let flash = new FlashMessage("Hello from ES2015, Babel and Gulp!");
+// flash.display();
